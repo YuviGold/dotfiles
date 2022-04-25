34b9ec37a2a2aa6088f0db396d9e30dcbd34cd88
Yuval Goldberg <yuvigoldi@gmail.com>
2022-04-25 19:14:41 +0300

    Add gnome extensions & vim plugins & GitHub test workflow

--- configs/.vimrc
+++ configs/.vimrc
@@ -1,3 +1,16 @@
+let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
+if empty(glob(data_dir . '/autoload/plug.vim'))
+  silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+endif
+
+call plug#begin()
+
+Plug 'dense-analysis/ale'
+Plug 'Yggdroot/indentLine'
+
+call plug#end()
+
 set number
 set showmatch
 set wildmenu
@@ -5,4 +18,7 @@ set cursorline
 set autoindent
 
 syntax on
+
+autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab
+autocmd FileType make set noexpandtab
 
--- install.sh
+++ install.sh
@@ -3,16 +3,31 @@
 set -o nounset
 set -o pipefail
 set -o errexit
+set -o xtrace
 
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
+	vim git curl wget \
+	gnome-shell-extensions terminator \
+	flameshot fzf autojump
 
-sudo apt install \
-	vim \
-	git \
-	curl
+# Vim
+vim -es -u configs/.vimrc -i NONE -c "PlugInstall" -c "qa"
+
+# Gnome extensions
+extensions=( "primary_input_on_lockscreensagidayan.com.v2" "wsmatrixmartin.zurowietz.de.v33" )
+for extension in "${extensions[@]}"; do
+	file="${extension}.shell-extension.zip"
+	wget "https://extensions.gnome.org/extension-data/${file}"
+	uuid=$(unzip -c "${file}" metadata.json | grep uuid | cut -d \" -f4)
+	mkdir -p "${HOME}/.local/share/gnome-shell/extensions/${uuid}"
+	unzip -q "${file}" -d "${HOME}/.local/share/gnome-shell/extensions/${uuid}/"
+	gnome-extensions enable "${uuid}"
+	rm -f "${file}"
+done
 
 # Terminal & Shell
-sudo apt install terminator
-if [ -z "${ZSH:-}" ]; then
+if [ -n "${ZSH:-}" ]; then
 	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	sed -i 's/^plugins=.*/plugins=(git sudo colored-man-pages dircycle)/g' "${HOME}/.zshrc"
 fi
@@ -22,23 +37,24 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 find "${__dir}/configs" -type f -exec sh -c 'ln -v -s {} ${HOME}/$(basename {})' \;
 
 # Source shell config
-rcfile="${HOME}/.$(echo ${SHELL} | cut -d'/' -f 4)rc"
+rcfile="${HOME}/.$(basename "${SHELL}")rc"
 grep "shellrc" "${rcfile}" > /dev/null || echo "source ${HOME}/.shellrc" >> "${rcfile}"
+source "${HOME}/.shellrc"
 
 # Install programs
 
 ## Package managers
-if [ ! -x "$(command -v brew)" ]; then
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-fi
-
 if [ ! -x "$(command -v arkade)" ]; then
 	curl -sLS https://get.arkade.dev | sudo sh
 fi
 
+arkade system install go
+
 ## Utilities
-brew install fzf
+wget https://github.com/dandavison/delta/releases/download/0.12.1/git-delta_0.12.1_amd64.deb
+sudo dpkg -i git-delta_0.12.1_amd64.deb
+rm -rf git-delta_0.12.1_amd64.deb
+
 arkade get jq
 
 ## K8s utils
@@ -46,6 +62,4 @@ arkade get docker-compose kubectl kind
 
 ## Git extensions
 arkade get gh
-gh extension install mislav/gh-branch
-brew install git-delta
-
+# gh extension install mislav/gh-branch
